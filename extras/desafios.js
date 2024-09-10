38602cc1da5f5cd1cde576c629dc1d56cfe358d5
AdrianoBispo <drianvini@10gmail.com>
2024-09-10 20:21:53 -0300

    Diretório 'extras' adicionado ao projeto

---
+++ extras/desafios.js
@@ -0,0 +1,73 @@
+/* Desafio I: Mostre um alerta com a mensagem "Boas vindas ao nosso site!". /
+
+alert('Boas vindas ao nosso site!');
+*/
+
+/* Desafio II: Declare uma variável chamada "nome" e atribua a ela o valor "Lua". /
+
+let nome = 'Lua'; 
+alert("Seja bem vinda, ${nomeDigitado}!");
+*/
+
+/* Desafio III: Crie uma variável chamada "idade" e atribua a ela o valor 25. /
+
+let idade = 25;
+alert("Sua idade é ${idade}")
+*/
+
+/* Desafio IV: Defina uma variável numeroDeVendas e atribua a ela o valor 50. /
+
+let numeroDeVendas = 50;
+alert("O seu número de vendas foi ${numeroDeVendas}");
+*/
+
+/* Desafio V: Defina uma variável saldoDisponivel e atribua a ela o valor 1000. /
+
+saldoDisponivel = 1000;
+alert("O seu saldo disponivel é de ${saldoDisponivel}");
+*/
+
+/* Desafio VI: Exiba um alerta com o texto "Erro! Preencha todos os campos" /
+
+alert('Erro! Preencha todos os campos!')
+*/
+
+/* Desafio VII: Declare uma variável chamada "mensagemDeErro" e atribua a ela o valor "Erro! Preencha todos os campos". Em seguida exiba um alerta com o valor da variável "mensagemDeErro". /
+
+let mensagemDeErro = 'Erro! Preencha todos os campos!';
+alert(mensagemDeErro);
+*/
+
+/* Desafio VIII: Use um prompt para perguntar o nome do usuário e armazená-lo na variável nome. /
+
+let nome = prompt("Qual é o seu nome?");
+*/
+
+/* Desafio IX: Peça ao usuário para digitar sua idade usando um prompt e armazene-a na variável idade. /
+
+let idade = prompt("Digite sua idade ");
+*/
+
+/* Desafio X: Agora, caso a idade seja maior ou igual que 18, exiba um alerta com a mensagem "Pode tirar a habilitação!" /
+let idade = prompt("Digite sua idade ");
+
+if (idade >= 18) {
+    alert('Você já pode tirar sua habilitação!')
+} else {
+    alert('Você não pode tirar sua habilitação!')
+}
+*/
+
+/* Exercício Trabalhando com Condicionais
+
+let porcentagemDesconto = 0;
+
+if(quantidadeMilhas >= 30000){
+    porcentagemDesconto = porcentagemDesconto + 20;
+} else {
+    if(quantidadeMilhas > 5000){
+    porcentagemDesconto = porcentagemDesconto + 10;
+}
+
+}
+*/
