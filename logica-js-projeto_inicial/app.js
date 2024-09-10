20b31bd221a5950cc630f650995a3ca88e927189
AdrianoBispo <drianvini@10gmail.com>
2024-09-10 20:46:04 -0300

    Adicionando a bilioteca matemática padrão do javascript para gerar o número aleatório no projeto.

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,16 +1,34 @@
-// Dar as boas vindas ao jogador.
+// Dar boas vindas ao usuario
 alert('Boas vindas ao jogo do número secreto');
 
-// Cria a variável e apresenta no console do navegador.
-let numeroSecreto = 9;
-console.log(numeroSecreto)
+// Cria o número aleatório através do método matemático padrão do javascript
+let numeroSecreto = parseInt(Math.random() * 10 + 1);
+console.log(numeroSecreto) // Informa o número secreto no console do navegador
 
-// Cria a variável "chute" que é responsável por receber o número digitado pelo jogador.
-let chute = prompt('Escolha um número entre 1 e 10');
+// Define as variáveis chute e tentativas
+let chute;
+let tentativas = 1;
 
-// Valida se o número digitado pelo jogador é o mesmo que o número secreto e retorna uma mensagem.
-if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
-} else {
-    alert('Você errou :(')
+// enquanto chute não for igual ao número secreto ele repetirá o prompt da variável "chute"
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 10');
+    
+    // se chute for igual ao número secreto ele encerra o loop
+    if (chute == numeroSecreto) {
+       break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`)
+        } else {
+            alert(`O número secreto é maior que ${chute}`)
+        }
+        
+        tentativas++ // tentativas = tentativas + 1
+    }
 }
+
+// Valida se o número de tentativas é maior que 1, caso seja, exibe 'tentativas'
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+
+// Imprime uma mensagem de felicitação caso o "chute" for igual a "númeroSecreto"
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
