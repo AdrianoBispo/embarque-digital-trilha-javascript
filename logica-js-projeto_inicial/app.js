b7c9a71cc4348b578de0c2b50f49fca4c301458b
AdrianoBispo <drianvini@10gmail.com>
2024-09-10 20:32:41 -0300

    Segunda lógica do projeto criada

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,8 +1,16 @@
+// Dar as boas vindas ao jogador.
 alert('Boas vindas ao jogo do número secreto');
 
-let numeroSecreto = 5;
+// Cria a variável e apresenta no console do navegador.
+let numeroSecreto = 9;
+console.log(numeroSecreto)
+
+// Cria a variável "chute" que é responsável por receber o número digitado pelo jogador.
 let chute = prompt('Escolha um número entre 1 e 10');
 
+// Valida se o número digitado pelo jogador é o mesmo que o número secreto e retorna uma mensagem.
 if (chute == numeroSecreto) {
-    console.log('Isso ai! Você descobriu o número secreto (5)');
+    alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto}`);
+} else {
+    alert('Você errou :(')
 }
