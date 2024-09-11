aeaa14adfc60290fbb27d5bdb11b0ffa234663da
AdrianoBispo <drianvini@10gmail.com>
2024-09-11 19:19:34 -0300

    Agora você é quem escolhe a dificuldade do jogo! Foi adicionada uma variável nova responsável por limitar o número número secreto ao seu valor máximo.

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -2,33 +2,34 @@
 alert('Boas vindas ao jogo do número secreto');
 
 // Cria o número aleatório através do método matemático padrão do javascript
-let numeroSecreto = parseInt(Math.random() * 100 + 1);
-console.log(numeroSecreto) // Informa o número secreto no console do navegador
+let numeroMaximo = prompt('Digite o número limitador máximo do jogo: ');
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
+console.log(numeroSecreto); // Informa o número secreto no console do navegador
 
 // Define as variáveis chute e tentativas
 let chute;
 let tentativas = 1;
 
-// enquanto chute não for igual ao número secreto ele repetirá o prompt da variável "chute"
+// Enquanto chute não for igual ao número secreto ele repetirá o prompt da variável "chute"
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um número entre 1 e 10');
+    chute = prompt(`Escolha um número entre 1 e ${numeroMaximo}`);
     
-    // se chute for igual ao número secreto ele encerra o loop
+    // Se chute for igual ao número secreto ele encerra o loop
     if (chute == numeroSecreto) {
        break;
     } else {
         if (chute > numeroSecreto) {
-            alert(`O número secreto é menor que ${chute}`)
+            alert(`O número secreto é menor que ${chute}`);
         } else {
-            alert(`O número secreto é maior que ${chute}`)
+            alert(`O número secreto é maior que ${chute}`);
         }
         
-        tentativas++ // tentativas = tentativas + 1
+        tentativas++; // tentativas = tentativas + 1
     }
 }
 
 // Valida se o número de tentativas é maior que 1, caso seja, exibe 'tentativas'
-let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa'
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
 
 // Imprime uma mensagem de felicitação caso o "chute" for igual a "númeroSecreto"
 alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}`);
